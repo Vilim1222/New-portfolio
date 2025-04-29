1d9d2d9b90ea58a6773ad6c2c29db58bc0382958
Vilim1222 <vilim.tavcar@gmail.com>
2025-04-29 09:06:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const categories = {
-  fashion: ['images/fashion1.jpg', 'images/fashion2.jpg', 'images/fashion3.jpg'],
-  nature: ['images/nature1.jpg', 'images/nature2.jpg'],
-  portraits: ['images/portraits1.jpg', 'images/portraits2.jpg'],
-  street: ['images/street1.jpg', 'images/street2.jpg'],
-  commercial: ['images/commercial1.jpg', 'images/commercial2.jpg']
+  fashion: ['images/fashion1.jpg', 'images/fashion2.jpg', 'images/fashion3.jpg', 'images/fashion4.jpg', 'images/fashion5.jpg', 'images/fashion6.jpg', 'images/fashion7.jpg', 'images/fashion8.jpg', 'images/fashion9.jpg', 'images/fashion10.jpg'],
+  nature: ['images/nature1.jpg', 'images/nature2.jpg', 'images/nature3.jpg', 'images/nature4.jpg', 'images/nature5.jpg', 'images/nature6.jpg', 'images/nature7.jpg', 'images/nature8.jpg', 'images/nature9.jpg', 'images/nature10.jpg'],
+  portraits: ['images/portraits1.jpg', 'images/portraits2.jpg', 'images/portraits3.jpg', 'images/portraits4.jpg', 'images/portraits5.jpg', 'images/portraits6.jpg', 'images/portraits7.jpg', 'images/portraits8.jpg', 'images/portraits9.jpg', 'images/portraits10.jpg'],
+  street: ['images/street1.jpg', 'images/street2.jpg', 'images/street3.jpg', 'images/street4.jpg', 'images/street5.jpg', 'images/street6.jpg', 'images/street7.jpg', 'images/street8.jpg', 'images/street9.jpg', 'images/street10.jpg'],
+  commercial: ['images/commercial1.jpg', 'images/commercial2.jpg', 'images/commercial3.jpg', 'images/commercial4.jpg', 'images/commercial5.jpg', 'images/commercial6.jpg', 'images/commercial7.jpg', 'images/commercial8.jpg', 'images/commercial9.jpg', 'images/commercial10.jpg']
 };
 
 let currentCategory = 'fashion';
